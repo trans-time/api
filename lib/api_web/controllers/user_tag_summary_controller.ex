86f0a60275d3dd23880d3f21f099101aa6c9022a
Patience Tema Baron <patience.tema.baron@gmail.com>
2018-04-28 11:57:08 -0400

    assort profile fixes and tweaks

--- lib/api_web/controllers/user_tag_summary_controller.ex
+++ lib/api_web/controllers/user_tag_summary_controller.ex
@@ -9,6 +9,10 @@ defmodule ApiWeb.UserTagSummaryController do
 
   def model, do: UserTagSummary
 
+  def filter(_conn, query, "author_id", author_id) do
+    where(query, author_id: ^author_id)
+  end
+
   def filter(_conn, query, "subject_id", subject_id) do
     where(query, subject_id: ^subject_id)
   end
